93784dbfde5295a5b76e738dba27e034bcdab2d4
Gabe Cook <gabe565@gmail.com>
2024-02-19 15:25:46 -0600

    fix: Fix dirs and supporting-services flags not being comma-separated

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -7,10 +7,10 @@ func New() *cobra.Command {
 		Use:  "template-kubernetes-apps-markdown",
 		RunE: run,
 	}
-	cmd.Flags().StringArrayVar(&dirs, "dirs", dirs, "Directories to template")
+	cmd.Flags().StringSliceVar(&dirs, "dirs", dirs, "Comma-separated list of directories to template")
 	cmd.Flags().StringVar(&file, "output", file, "Output filename")
 	cmd.Flags().StringVar(&startTag, "start-tag", startTag, "Markdown tag that begins replacement")
 	cmd.Flags().StringVar(&endTag, "end-tag", endTag, "Markdown tag that ends replacement")
-	cmd.Flags().StringArrayVar(&supportingServices, "supporting-services", supportingServices, "Names of supporting services")
+	cmd.Flags().StringSliceVar(&supportingServices, "supporting-services", supportingServices, "Comma-separated list of supporting service names")
 	return cmd
 }
