82097751b5e5c398e0b8577ec3ce10dc696829d9
Gabe Cook <gabe565@gmail.com>
2024-07-22 15:55:21 -0500

    feat: Include `StatefulSet` manifests

--- cmd/run.go
+++ cmd/run.go
@@ -142,6 +142,7 @@ func walkFunc(matchCh chan Match) filepath.WalkFunc {
 				case slices.Contains(excludedServices, name):
 					continue
 				case apiVersion == "apps/v1" && kind == "Deployment":
+				case apiVersion == "apps/v1" && kind == "StatefulSet":
 				case apiVersion == "batch/v1" && kind == "CronJob":
 				case strings.HasPrefix(apiVersion, "helm.toolkit.fluxcd.io") && kind == "HelmRelease":
 				case strings.HasPrefix(apiVersion, "source.toolkit.fluxcd.io") && kind == "GitRepository" && name != "flux-system":
